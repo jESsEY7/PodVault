e9d1e3fdf0204239aeb5362f9f4a1117e9b16699
jESsEY7 <ngenojesse@gmail.com>
2026-02-17 09:46:52 +0300

    Fix ALLOWED_HOSTS to read from environment variable

--- backend/podvault_api/settings.py
+++ backend/podvault_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-c9asdyoqrk4h*^mylh!1$new#m!b#+r$h#!!og&en#v+b@^*0(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
